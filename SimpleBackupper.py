c97a71d17a3c1933ae69de1151d6c0dad8b5c871
alexodee <alexandredutech89@gmail.com>
2023-08-20 17:58:39 +0200

    Initial commit

--- SimpleBackupper.py
+++ SimpleBackupper.py
@@ -1,64 +1,50 @@
 import sys
 import os
-import shutil
 import json
-from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QFileDialog, QMessageBox, QProgressBar, QComboBox, QMenuBar, QMenu, QAction)
+import subprocess
+import datetime
+from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QFileDialog, QMessageBox, QProgressBar, QComboBox, QSystemTrayIcon, QMenu, QAction)
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QTimer
 
 class SimpleBackupper(QWidget):
-    CONFIG_FILE = "backup_config.json"
-    LANG_DIR = "lang"
+    SETTINGS_FILE = "settings.json"
 
     def __init__(self):
         super().__init__()
-        self.loadConfig()
+        self.loadSettings()
         self.initUI()
+        self.setupBackupScheduler()
+        self.tray_icon = QSystemTrayIcon(QIcon("ico.png"), self)
+        self.setupTrayIcon()
+        self.setWindowIcon(QIcon("ico.png"))
 
     def initUI(self):
         self.setWindowTitle("SimpleBackupper")
         self.setGeometry(100, 100, 400, 300)
-        self.setWindowIcon(QIcon('ico.png'))
 
         layout = QVBoxLayout()
-
-        # Menu for language selection
-        self.menu_bar = QMenuBar()
-
-        self.language_menu = QMenu(self)
-        self.french_action = QAction(self)
-        self.french_action.triggered.connect(lambda: self.set_language("fr"))
-        self.english_action = QAction(self)
-        self.english_action.triggered.connect(lambda: self.set_language("en"))
-        self.language_menu.addAction(self.french_action)
-        self.language_menu.addAction(self.english_action)
-        self.menu_bar.addMenu(self.language_menu)
-
-        layout.setMenuBar(self.menu_bar)
-
-        # UI elements for backup functionality
-        self.source_label = QLabel(self)
-        self.source_lineedit = QLineEdit(self.config.get("source_dir", "C:\\Users"))
-        self.source_lineedit.textChanged.connect(self.saveConfig)
-        self.source_button = QPushButton(self)
+        self.source_label = QLabel("Source Directory", self)
+        self.source_lineedit = QLineEdit(self.settings.get("source_dir", ""), self)
+        self.source_lineedit.textChanged.connect(self.saveSettings)
+        self.source_button = QPushButton("Browse", self)
         self.source_button.clicked.connect(self.selectSourceDirectory)
-
-        self.destination_label = QLabel(self)
-        self.destination_lineedit = QLineEdit(self.config.get("dest_dir", ""))
-        self.destination_lineedit.textChanged.connect(self.saveConfig)
-        self.destination_button = QPushButton(self)
-        self.destination_button.clicked.connect(self.selectDestinationPath)
-
-        self.interval_label = QLabel(self)
+        self.destination_label = QLabel("Destination Directory", self)
+        self.destination_lineedit = QLineEdit(self.settings.get("dest_dir", ""), self)
+        self.destination_lineedit.textChanged.connect(self.saveSettings)
+        self.destination_button = QPushButton("Browse", self)
+        self.destination_button.clicked.connect(self.selectDestinationDirectory)
+        self.interval_label = QLabel("Backup Interval", self)
         self.interval_combobox = QComboBox(self)
-        self.interval_combobox.addItems(["Continuous", "Daily", "Weekly"])
-        self.interval_combobox.currentIndexChanged.connect(self.saveConfig)
-
-        self.backup_button = QPushButton(self)
-        self.backup_button.clicked.connect(self.performBackup)
-
+        self.interval_combobox.addItems(["None", "Continuous (10 minutes)", "Daily", "Weekly", "Monthly"])
+        self.interval_combobox.setCurrentText(self.settings.get("interval", "None"))
+        self.interval_combobox.currentIndexChanged.connect(self.updateBackupInterval)
+        self.backup_button = QPushButton("Backup Now", self)
+        self.backup_button.clicked.connect(lambda: self.performBackup(manual=True))
         self.progress_bar = QProgressBar(self)
         self.progress_bar.setValue(0)
+        self.last_backup_label = QLabel("Last backup: " + self.settings.get("last_backup", "N/A"))
+        self.next_backup_label = QLabel("Next backup: Calculating...")
 
         layout.addWidget(self.source_label)
         layout.addWidget(self.source_lineedit)
@@ -70,76 +56,90 @@ class SimpleBackupper(QWidget):
         layout.addWidget(self.interval_combobox)
         layout.addWidget(self.backup_button)
         layout.addWidget(self.progress_bar)
-
+        layout.addWidget(self.last_backup_label)
+        layout.addWidget(self.next_backup_label)
         self.setLayout(layout)
 
-        self.backup_timer = QTimer(self)
-        self.backup_timer.timeout.connect(self.performBackup)
-
-        # Load default language or saved one
-        self.set_language(self.config.get("language", "en"))
-
-    def loadConfig(self):
-        if os.path.exists(self.CONFIG_FILE):
-            with open(self.CONFIG_FILE, 'r') as file:
-                self.config = json.load(file)
+    def loadSettings(self):
+        if os.path.exists(self.SETTINGS_FILE):
+            with open(self.SETTINGS_FILE, 'r') as file:
+                self.settings = json.load(file)
         else:
-            self.config = {}
+            self.settings = {}
 
-    def saveConfig(self):
-        with open(self.CONFIG_FILE, 'w') as file:
-            self.config["source_dir"] = self.source_lineedit.text()
-            self.config["dest_dir"] = self.destination_lineedit.text()
-            self.config["interval"] = self.interval_combobox.currentText()
-            json.dump(self.config, file)
-
-    def set_language(self, lang):
-        """Update UI text based on language."""
-        self.config["language"] = lang
-        self.saveConfig()
-
-        # Load translations from the file with explicit encoding
-        with open(os.path.join(self.LANG_DIR, f"{lang}.json"), 'r', encoding='utf-8') as f:
-            translations = json.load(f)
-
-        tr = translations[lang]
-
-        self.source_label.setText(tr["source"])
-        self.source_button.setText(tr["browse"])
-        self.destination_label.setText(tr["destination"])
-        self.destination_button.setText(tr["browse"])
-        self.interval_label.setText(tr["interval"])
-        self.interval_combobox.setItemText(0, tr["continuous"])
-        self.interval_combobox.setItemText(1, tr["daily"])
-        self.interval_combobox.setItemText(2, tr["weekly"])
-        self.backup_button.setText(tr["backup"])
-
-        self.language_menu.setTitle(tr["language"])
-        self.french_action.setText(tr["french"])
-        self.english_action.setText(tr["english"])
+    def saveSettings(self):
+        with open(self.SETTINGS_FILE, 'w') as file:
+            self.settings["source_dir"] = self.source_lineedit.text()
+            self.settings["dest_dir"] = self.destination_lineedit.text()
+            self.settings["interval"] = self.interval_combobox.currentText()
+            json.dump(self.settings, file)
 
     def selectSourceDirectory(self):
         source_dir = QFileDialog.getExistingDirectory(self, "Select Source Directory", self.source_lineedit.text())
         if source_dir:
             self.source_lineedit.setText(source_dir)
 
-    def selectDestinationPath(self):
-        dest_dir = QFileDialog.getExistingDirectory(self, "Select Destination Path", self.destination_lineedit.text())
+    def selectDestinationDirectory(self):
+        dest_dir = QFileDialog.getExistingDirectory(self, "Select Destination Directory", self.destination_lineedit.text())
         if dest_dir:
             self.destination_lineedit.setText(dest_dir)
 
-    def performBackup(self):
-        # Backup logic (Copying directory as an example)
+    def updateBackupInterval(self):
+        interval_mappings = {
+            "None": None,
+            "Continuous (10 minutes)": 600000,
+            "Daily": 86400000,
+            "Weekly": 604800000,
+            "Monthly": 2592000000
+        }
+        interval = self.interval_combobox.currentText()
+        if interval != "None":
+            self.backup_timer.setInterval(interval_mappings[interval])
+            self.backup_timer.start()
+            next_backup = datetime.datetime.now() + datetime.timedelta(milliseconds=interval_mappings[interval])
+            self.next_backup_label.setText("Next backup: " + next_backup.strftime("%d-%m-%Y %H:%M:%S"))
+        else:
+            self.backup_timer.stop()
+            self.next_backup_label.setText("No scheduled backups")
+        self.saveSettings()
+
+    def performBackup(self, manual=False):
         source = self.source_lineedit.text()
-        destination = os.path.join(self.destination_lineedit.text(), os.path.basename(source))
-        
-        try:
-            shutil.copytree(source, destination)
-            QMessageBox.information(self, "Backup", "Backup completed successfully!")
-            self.progress_bar.setValue(100)
-            QTimer.singleShot(10000, lambda: self.progress_bar.setValue(0))
-        except Exception as e:
-            QMessageBox.critical(self, "Error", f"Backup failed: {e}")
+        destination = self.destination_lineedit.text()
+        cmd = ["robocopy", source, destination, "/MIR"]
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = process.communicate()
+        print(out.decode("utf-8", errors='replace'))
+        self.progress_bar.setValue(100)
+        self.settings["last_backup"] = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+        self.last_backup_label.setText("Last backup: " + datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S"))
+        if manual:
+            QMessageBox.information(self, "Backup Completed", "Backup was successful!")
+        next_interval = self.backup_timer.interval()
+        if next_interval:
+            next_backup = datetime.datetime.now() + datetime.timedelta(milliseconds=next_interval)
+            self.next_backup_label.setText("Next backup: " + next_backup.strftime("%d-%m-%Y %H:%M:%S"))
+        self.saveSettings()
+
+    def setupBackupScheduler(self):
+        self.backup_timer = QTimer(self)
+        self.backup_timer.timeout.connect(self.performBackup)
+        self.updateBackupInterval()
+
+    def setupTrayIcon(self):
+        menu = QMenu()
+        show_action = QAction("Show", self)
+        quit_action = QAction("Exit", self)
+        show_action.triggered.connect(self.show)
+        quit_action.triggered.connect(QApplication.quit)
+        menu.addAction(show_action)
+        menu.addAction(quit_action)
+        self.tray_icon.setContextMenu(menu)
+        self.tray_icon.show()
+
+    def closeEvent(self, event):
+        event.ignore()
+        self.hide()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
---
+++ readme.md
@@ -0,0 +1,35 @@
+# SimpleBackupper
+
+<img src="ico.png" width="100" alt="SimpleBackupper Icon">
+
+**SimpleBackupper** is a packaged Python application offering a straightforward and user-friendly backup solution. Utilizing Robocopy under the hood, it ensures a dependable and efficient backup from your source path to your target path.
+
+## 🌟 Features
+
+- **User-Friendly Interface**: A simplified interface, making your file backup automatic and as easy as a few clicks.
+- **Robocopy Under the Hood**: Harness the power and reliability of Robocopy for your backups.
+- **Backup Scheduling**: Automate your backups at different intervals according to your needs.
+- **Effortless Installation**: With our installer, get SimpleBackupper up and running in moments.
+
+## 🚀 Getting Started
+
+### Prerequisites
+
+- **Windows 7/10/11** (due to the Robocopy dependency).
+
+### Installation
+
+For an easy installation, head over to our [releases page](your_link_to_releases) and download the latest version of the installer.
+
+1. Run the installer and follow the simple steps.
+2. Once the installation is complete, launch SimpleBackupper and start backing up!
+
+## 📸 Screenshots
+
+<img src="screenshots/screen_1.png" width="200" alt="Screenshot 1">
+
+## 📜 License
+
+This project is licensed under the MIT license - see the [LICENSE.md](LICENSE.md) file for more details.
+
+Made in 🇫🇷
